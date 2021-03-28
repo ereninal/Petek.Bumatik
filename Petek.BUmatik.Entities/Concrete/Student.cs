1185b31e6ced6db272b3da1863108b4f607ba41b
Eren İNAL <ereninal10@gmail.com>
2021-03-28 18:09:03 +0300

    Updated.

--- Petek.BUmatik.Entities/Concrete/Student.cs
+++ Petek.BUmatik.Entities/Concrete/Student.cs
@@ -9,14 +9,14 @@ namespace Petek.BUmatik.Entities.Concrete
     {
         public int Id { get; set; }
         public string  FullName { get; set; }
-        public byte[] Image{ get; set; }
+        public byte[]? Image{ get; set; }
         public decimal RemainMoney { get; set; }
         public string BandNumber { get; set; }
-        public DateTime BirthDate { get; set; }
+        public DateTime? BirthDate { get; set; }
         public DateTime CreatedDate { get; set; }
-        public DateTime ModifiedDate { get; set; }
-        public int CreatedUserId { get; set; }
-        public int ModifieUserId { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public int? CreatedUserId { get; set; }
+        public int? ModifieUserId { get; set; }
         public bool IsDeleted { get; set; }
         public int SchoolId { get; set; }
         public int ParentId { get; set; }
